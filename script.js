d066e47e933253f32e06093f361b446a5f4f05cd
TheBtechGuy26 <157542689+TheBtechGuy26@users.noreply.github.com>
2024-02-08 19:27:53 +0000

    Done

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 4,
     centeredSlides: true,
-    spaceBetween: 30,
+    spaceBetween: 10,
+    loop: true,
+   centerSlide: 'true',
+   fade: 'true',
     grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      dynamicBullets: true,
     },
   });
 
@@ -14,13 +18,35 @@ var swiper = new Swiper(".slide-content", {
 //     spaceBetween: 30,
 //     slidesPerGroup: 3,
 //     loop: true,
+//     centerSlide: 'true',
+//     fade: 'true',
+//     grabCursor: 'true',
 //     loopFillGroupWithBlank: true,
 //     pagination: {
 //         el: ".swiper-pagination",
 //         clickable: true,
+//         dynamicBullets: true,
 //     },
 //     navigation: {
-//         nextE1: ".swiper-button-next",
-//         prevE1: ".swiper-button-prev",
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+// var swiper = new Swiper(".slide-content", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     slidesPerGroup: 3,
+//     loop: true,
+//     centeredSlides: true, // Use centeredSlides instead of centerSlide
+//     slideToClickedSlide: true, // To slide to the clicked slide
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
 //     },
 // });
